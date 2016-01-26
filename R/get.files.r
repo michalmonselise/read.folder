efe7e3f114e5ee50b994245f993985f4d650e535
Michal Monselise <michal.monselise@ds-iq.com>
2016-01-26 09:56:25 -0800

    resolving get.files recursive issue

--- R/get.files.r
+++ R/get.files.r
@@ -17,5 +17,6 @@ get.files <- function(folder.path, recursive=FALSE) {
             all.files <- c(all.files, fls)
             }
         }
+    all.files <- all.files[file.info(all.files)$size > 0]
     return(all.files)
 }    
